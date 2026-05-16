1322d745d1e1c7ab5e8ad28c328e94daf1e1d0f5
Anderson Mattos <andersonmattos5555@gmail.com>
2026-05-16 14:08:13 -0300

    atualização do site

--- src/app/page.js
+++ src/app/page.js
@@ -197,7 +197,7 @@ export default function Home() {
         id: 5,
         nome: "Elos",
         top: "40%",
-        left: "60%",
+        left: "50%",
 
         lazerExterno: [
           "Piscina panorâmica",
