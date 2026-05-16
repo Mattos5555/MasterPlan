80c65e3ba2cb1c4ecd2b019de0f82c142f11ba9b
Anderson Mattos <andersonmattos5555@gmail.com>
2026-05-16 16:22:35 -0300

    atualização do site

--- src/app/page.js
+++ src/app/page.js
@@ -238,7 +238,7 @@ export default function Home() {
         id: 6,
         nome: "Pura",
         top: "50%",
-        left: "55%",
+        left: "25%",
 
         lazerExterno: [
           "Piscina natural",
